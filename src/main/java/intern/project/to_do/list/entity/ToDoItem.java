83d7f3fb01c0a730e6a2da241bae4a8c42e6d684
Nureles <almazbekuulunureles5@gmail.com>
2024-09-26 17:11:21 +0600

    feat:added validations and exception handlings

--- src/main/java/intern/project/to_do/list/entity/ToDoItem.java
+++ src/main/java/intern/project/to_do/list/entity/ToDoItem.java
@@ -1,34 +1,38 @@
 package intern.project.to_do.list.entity;
-
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
-
 import java.time.LocalDateTime;
+
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Getter
-@Setter
 public class ToDoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Title is mandatory")
+    @Size(min = 1, max = 50, message = "Title must be between 1 and 50 characters")
     @Column(nullable = false)
     private String title;
 
+    @Size(max = 200, message = "Description must be up to 200 characters")
     private String description;
 
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "Status is mandatory")
     @Column(nullable = false)
     private TaskStatus status = TaskStatus.NOT_STARTED;
 
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
 
+    @FutureOrPresent(message = "Due date cannot be in the past")
     private LocalDateTime dueDate;
 
     @PrePersist
